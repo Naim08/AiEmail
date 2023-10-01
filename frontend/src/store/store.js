e8b0983d62bf7aff0406dd116adade486c4f350b
Naim Miah <mmiah@fordham.edu>
2023-10-01 13:59:38 -0400

    added DS_Store to gitignore

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,21 +1,19 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import errors from './errors';
-import tweetsReducer from './tweets';
-import sessionReducer from './session';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import errors from "./errors";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   errors,
-  tweets: tweetsReducer
 });
 
 let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   enhancer = applyMiddleware(thunk);
 } else {
-  const logger = require('redux-logger').default;
+  const logger = require("redux-logger").default;
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
