19ca6383196ea6299562af36469831eb6bf40e18
Naim Miah <mmiah@fordham.edu>
2023-10-02 02:41:35 -0400

    added chatgpt api

---
+++ frontend/src/store/chatgpt.js
@@ -0,0 +1,110 @@
+import jwtFetch from "./jwt";
+
+const SEND_MESSAGE = "chatgpt/SEND_MESSAGE";
+const RECEIVE_MESSAGE = "chatgpt/RECEIVE_MESSAGE";
+const RECEIVE_MESSAGES = "chatgpt/RECEIVE_MESSAGES";
+const RECEIVE_MESSAGE_ERRORS = "chatgpt/RECEIVE_MESSAGE_ERRORS";
+const CLEAR_MESSAGE_ERRORS = "chatgpt/CLEAR_MESSAGE_ERRORS";
+
+const receiveMessage = (message) => ({
+  type: RECEIVE_MESSAGE,
+  message,
+});
+
+const receiveMessages = (messages) => ({
+  type: RECEIVE_MESSAGES,
+  messages,
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_MESSAGE_ERRORS,
+  errors,
+});
+
+export const clearMessageErrors = () => ({
+  type: CLEAR_MESSAGE_ERRORS,
+});
+
+export const sendMessage = (message) => async (dispatch) => {
+  try {
+    const res = await jwtFetch("/api/chatgpt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+    const responseData = await res.json; // Renamed the constant
+    dispatch(receiveMessage(responseData));
+  } catch (err) {
+    if (err.response) {
+      // Check if response exists in error (it might not for network errors)
+      const statusCode = err.response.status;
+      const resBody = err.response.data;
+
+      if (statusCode === 400) {
+        dispatch(receiveErrors(resBody.errors));
+      } else if (statusCode === 500) {
+        dispatch(receiveErrors(resBody.errors));
+      }
+    } else {
+      // If there is no response, it's a network error
+      dispatch(receiveErrors(["A network error occurred."]));
+      console.log(err);
+    }
+  }
+};
+
+export const fetchMessages = () => async (dispatch) => {
+  try {
+    const res = await jwtFetch("/api/chatgpt");
+    const messages = await res.data;
+    dispatch(receiveMessages(messages));
+  } catch (err) {
+    const resBody = await err.json();
+    if (resBody.statusCode === 400) {
+      dispatch(receiveErrors(resBody.errors));
+    } else if (resBody.statusCode === 500) {
+      dispatch(receiveErrors(resBody.errors));
+    }
+  }
+};
+
+const initialState = {
+  messages: [],
+  errors: [],
+};
+
+const chatgptReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, action.message],
+      };
+    case RECEIVE_MESSAGES:
+      return {
+        ...state,
+        messages: action.messages,
+      };
+    case RECEIVE_MESSAGE_ERRORS:
+      return {
+        ...state,
+        errors: action.errors,
+      };
+    case CLEAR_MESSAGE_ERRORS:
+      return {
+        ...state,
+        errors: [],
+      };
+    case SEND_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, action.message],
+      };
+    default:
+      return state;
+  }
+};
+
+export default chatgptReducer;
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,41 +1,41 @@
-import jwtFetch from './jwt';
+import jwtFetch, { jwtAxiosFetch } from "./jwt";
 
 const RECEIVE_CURRENT_USER = "session/RECEIVE_CURRENT_USER";
 const RECEIVE_SESSION_ERRORS = "session/RECEIVE_SESSION_ERRORS";
 const CLEAR_SESSION_ERRORS = "session/CLEAR_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "session/RECEIVE_USER_LOGOUT";
 
-const receiveCurrentUser = currentUser => ({
+const receiveCurrentUser = (currentUser) => ({
   type: RECEIVE_CURRENT_USER,
-  currentUser
+  currentUser,
 });
-  
-const receiveErrors = errors => ({
+
+const receiveErrors = (errors) => ({
   type: RECEIVE_SESSION_ERRORS,
-  errors
+  errors,
 });
 
 const logoutUser = () => ({
-  type: RECEIVE_USER_LOGOUT
+  type: RECEIVE_USER_LOGOUT,
 });
 
 export const clearSessionErrors = () => ({
-  type: CLEAR_SESSION_ERRORS
+  type: CLEAR_SESSION_ERRORS,
 });
 
-export const signup = user => startSession(user, 'api/users/register');
-export const login = user => startSession(user, 'api/users/login');
+export const signup = (user) => startSession(user, "api/users/register");
+export const login = (user) => startSession(user, "api/users/login");
 
-const startSession = (userInfo, route) => async dispatch => {
-  try {  
+const startSession = (userInfo, route) => async (dispatch) => {
+  try {
     const res = await jwtFetch(route, {
       method: "POST",
-      body: JSON.stringify(userInfo)
+      body: JSON.stringify(userInfo),
     });
     const { user, token } = await res.json();
-    localStorage.setItem('jwtToken', token);
+    localStorage.setItem("jwtToken", token);
     return dispatch(receiveCurrentUser(user));
-  } catch(err) {
+  } catch (err) {
     const res = await err.json();
     if (res.statusCode === 400) {
       return dispatch(receiveErrors(res.errors));
@@ -43,25 +43,46 @@ const startSession = (userInfo, route) => async dispatch => {
   }
 };
 
-export const logout = () => dispatch => {
-  localStorage.removeItem('jwtToken');
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("jwtToken");
   dispatch(logoutUser());
 };
 
-export const getCurrentUser = () => async dispatch => {
-  const res = await jwtFetch('/api/users/current');
+export const getCurrentUser = () => async (dispatch) => {
+  const res = await jwtFetch("/api/users/current");
   const user = await res.json();
   return dispatch(receiveCurrentUser(user));
 };
 
+export const getAxiosCurrentUser = () => async (dispatch) => {
+  const res = await jwtAxiosFetch("/api/users/current");
+  const user = await res.data;
+  return dispatch(receiveCurrentUser(user));
+};
+
+export const googleLogin = (accessToken) => async (dispatch) => {
+  const res = await jwtAxiosFetch("/api/users/google-login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: {
+      access_token: accessToken,
+    },
+  });
+  const { user, token } = await res.data;
+  localStorage.setItem("jwtToken", token);
+  return dispatch(receiveCurrentUser(user));
+};
+
 const initialState = {
-  user: undefined
+  user: undefined,
 };
 
 const nullErrors = null;
 
 export const sessionErrorsReducer = (state = nullErrors, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
     case RECEIVE_CURRENT_USER:
