2005c6114e1b26fc4a14e46b23644a6717c06861
Foris8 <foristang@gmail.com>
2023-10-04 13:25:21 -0400

    finish the routes and delete button

--- frontend/src/components/Email/EmailDetails/index.jsx
+++ frontend/src/components/Email/EmailDetails/index.jsx
@@ -4,49 +4,86 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchSingleEmail } from '../../../store/email';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { updateEmail, deleteEmail } from '../../../store/email';
-import EmailForm from '../EmailForm';
+import { useHistory } from "react-router-dom";
 
 
 const EmailDetails = () => {
     const { emailId } = useParams();
-    const dispatch = useDispatch();
-    const email = useSelector(state => state.emailsReducer.emails.find(e => e._id === emailId));
     const isLoading = useSelector(state => state.emailsReducer.isLoading);
-    const [emailToUpdate, setEmailToUpdate] = useState({ subject: email.subject, message: email.message, _id: emailId });
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [email, setEmail] = useState(useSelector(state => state.emailsReducer.emails.find(e => e._id === emailId)))
 
-    useEffect(() => {
-        if (!email) {
-            dispatch(fetchSingleEmail(emailId));
-        }
-    }, [dispatch, email, emailId]);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEmail({ ...email, [name]: value });
+    };
 
-    useEffect(() => {
-        setEmailToUpdate({ 
-            subject: email?.subject, 
-            message: email?.message, 
-            _id: emailId 
-        });
-    }, [email, emailId]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(updateEmail({ ...email, id: emailId }));
+    };
+
+    const handleExit = (e) => {
+        e.preventDefault();
+        history.push(`/dashpage`);
+    }
 
 
     
 
     return (
-        <div>
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : email ? ( // Check if email exists before rendering its properties
-                <div>
-                    <h2>{email.subject}</h2>
-                    <p>{email.message}</p>
-                    <EmailForm emailToUpdate={emailToUpdate}/>
-                </div>
-            
-            ) : (
-                <p>Email not found</p>
-            )}
+        // <div className='new-email-form-page'>
+        <>
+        <div className='exit-page-btn-div'>
+        <div className="exit-wrapper" onClick={handleExit}>
+            <i className="fa-regular fa-arrow-left exit-icon"></i>
+            <p className="exit-text">Return to Dashboard</p>
         </div>
-    );
+        </div>
+
+        <div className='new-email-form-container'>
+            
+            <form onSubmit={handleSubmit} className='new-email-form'>
+            {/* <label>To:</label> */}
+            <div className='new-email-to'>
+                <input
+                type="text"
+                name="to"
+                placeholder="To"
+                />
+            </div>
+            <div className="dotted-line"></div>
+            {/* <label>Subject:</label> */}
+            <div className='new-email-subject'>
+                <input
+                type="text"
+                name="subject"
+                value={email.subject}
+                onChange={handleChange}
+                placeholder="Subject"
+                />
+            </div>
+            <div className="dotted-line"></div>
+            {/* <label>Body:</label> */}
+            <div className='new-email-body' style={{ flexGrow: 1 }}>
+                <textarea
+                name="message"
+                value={email.message}
+                onChange={handleChange}
+                placeholder="Message"
+                style={{ height: '100%' }}
+                />
+            </div>
+            <div className='new-email-form-btn'>
+                <button type="submit">update</button>
+            </div>
+            </form>
+        </div>
+        <div className="vertical-line"></div>
+
+        </>
+  );
 };
 
 export default EmailDetails;
