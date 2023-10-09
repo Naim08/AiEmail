13c81222fcd2ea7d51fe7929c60c70c185798217
elevenstand7 <elevenstand7@gmail.com>
2023-10-09 11:13:22 -0400

    finished the about-us

--- frontend/src/components/AboutUs/AboutUs.js
+++ frontend/src/components/AboutUs/AboutUs.js
@@ -1,30 +1,49 @@
 import "./AboutUs.css";
+import { useHistory } from "react-router-dom";
+import Sanjid from './pics/SANJID.png';
+import Fanyi from './pics/Fanyi.png';
+import Naim from './pics/Naim.jpeg';
+import Yinyin from './pics/Yinyin.png';
 
 const AboutUs = ()=>{
+    const history = useHistory();
+
+    const handleExit = () => {
+        history.push(`/dashpage`);
+    };
+
     return (
-        <div className="team-members-container">
-            <div className="member-box">
-                <div className="member-pic"></div>
-                <p>Fanyi Tang</p>
-                <p>Team Lead</p>
+        <div>
+            <div className="exit-page-btn-div">
+                <div className="exit-wrapper" onClick={handleExit}>
+                    <i className="fa-regular fa-arrow-left exit-icon"></i>
+                    <p className="exit-text">Return to Dashboard</p>
+                </div>
             </div>
+            <div className="team-members-container">
+                <div className="member-box">
+                    <img src={Fanyi} className="member-pic" />
+                    <p>Fanyi Tang</p>
+                    <p>Team Lead</p>
+                </div>
 
-            <div className="member-box">
-                <div className="member-pic"></div>
-                <p>Sanjid Dewan</p>
-                <p>Frontend Lead</p>
-            </div>
+                <div className="member-box">
+                    <img src={Sanjid} className="member-pic" />
+                    <p>Sanjid Dewan</p>
+                    <p>Frontend Lead</p>
+                </div>
 
-            <div className="member-box">
-                <div className="member-pic"></div>
-                <p>Naim Miah</p>
-                <p>Backend Lead</p>
-            </div>
+                <div className="member-box">
+                <img src={Naim} className="member-pic" />
+                    <p>Naim Miah</p>
+                    <p>Backend Lead</p>
+                </div>
 
-            <div className="member-box">
-                <div className="member-pic"></div>
-                <p>Yinyin Huang</p>
-                <p>Flex Lead</p>
+                <div className="member-box">
+                <img src={Yinyin} className="member-pic" />
+                    <p>Yinyin Huang</p>
+                    <p>Flex Lead</p>
+                </div>
             </div>
         </div>
     )
