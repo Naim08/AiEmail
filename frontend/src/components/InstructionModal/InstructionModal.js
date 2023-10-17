bc7d4eb7c1cd34d7148925c9a2ad49860e354bae
elevenstand7 <elevenstand7@gmail.com>
2023-10-16 21:45:55 -0400

    modify instruction content

--- frontend/src/components/InstructionModal/InstructionModal.js
+++ frontend/src/components/InstructionModal/InstructionModal.js
@@ -20,13 +20,35 @@ const InstructionModal = ({isActive, onClose, header}) =>{
                             <li>Click on it, you'll be prompted to log in to your Gmail account and authorize MailTo to manage your emails.</li>
                         </div>
                         <br />
-                        <div>Feature 2: Create new Email Responses
-                            <li>Click on the "+" button in the dash page, you can create a new email input page. </li>
-                            <li>Edit the corresponding position, and </li>
+
+                        <div>Feature 2: Create Email Responses
+                            <li>Click on the "+" button on the dash page, you can create a new email input page. </li>
+                            <li>Edit the corresponding position, and click the right bottom button {">"}, and you will send your content to AI API.</li>
+                            <li>Wait for a while, you will receive the response from AI API. You can copy the response by clicking the copy button at the right bottom of the response.</li>
+                            <li>You can open up a user preference modal by clicking the adjust button at the right-top of the message field.</li>
+                            <li>Through the user preference modal, you can adjust the content of the AI responses.</li>
                         </div>
                         <br />
-                        <div>ins1</div>
-                        <div>ins1</div>
+
+                        <div>Feature 3: Edit and Delete Previous Email Responses
+                            <li>In the dash page, you can find your previous emails.</li>
+                            <li>You can edit your previous emails to get the new AI responses by click into th email body, and edit the content, then click the update button.</li>
+                            <li>You can delete them by click on th trash button on each email's right bottom.</li>
+                        </div>
+                        <br />
+
+                        <div>Feature 4: Find your Delete Emails
+                            <li>You can find all the emails deleted when you click the trash button in the Navigation Bar.</li>
+                            <li>You can delete all the emails in the trash or restore them one by one.</li>
+                        </div>
+                        <br />
+
+                        <div>Feature 5: Send your email
+                            <li>Once you get the response from the AI, you will have two options: update your message to get a new response or copy the current response and send it to someone</li>
+                            <li>You can use the "update" button at the right bottom of the message body to get a new response.</li>
+                            <li>Or you can use the "send" button at the right bottom of the message body to send out your email.</li>
+                        </div>
+                        <br />
                     </div>
                 </div>
                 <div modal-footer>
