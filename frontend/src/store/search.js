c11b288899afedb3ba6e40021091684e01c32d7d
elevenstand7 <elevenstand7@gmail.com>
2023-10-23 14:49:04 -0700

    add user id to search route fix search loading problem

--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -2,6 +2,7 @@ import jwtFetch from "./jwt";
 
 export const GET_SEARCH_RESULTS = 'search/searchResults';
 export const CLEAR_SEARCH_RESULTS = 'search/clearSearchResults';
+export const SET_SEARCH_LOADING = 'search/setSearchLoading';
 
 export const receiveSearchResults = searchResults => ({
     type: GET_SEARCH_RESULTS,
@@ -11,23 +12,33 @@ export const clearSearchResults = () => ({
     type: CLEAR_SEARCH_RESULTS
 });
 
+export const setSearchLoading = isLoading => ({
+    type: SET_SEARCH_LOADING,
+    isLoading
+});
+
 export const fetchSearchResults = (query) => async dispatch => {
     //res route may need to change
+    // debugger
+    dispatch(setSearchLoading(true));
     const res = await jwtFetch(`/api/emails/search?query=${query}`);
     // debugger
     const emails = await res.json();
     dispatch(receiveSearchResults(emails));
+    dispatch(setSearchLoading(false));
 }
 
 
-const searchReducer = (state = {}, action) => {
+const searchReducer = (state = { isLoading: false }, action) => {
     const newState = {...state}
     switch (action.type) {
         case GET_SEARCH_RESULTS:
             // debugger
-            return {...action.searchResults};
+            return {...action.searchResults, isLoading: false };
         case CLEAR_SEARCH_RESULTS:
-            return {};
+            return { isLoading: false };
+        case SET_SEARCH_LOADING:
+            return { ...state, isLoading: action.isLoading };
         default:
             return newState;
     }
