a3cf16e3e081433890b63c4c75245352d59ca7ae
Naim Miah <mmiah@fordham.edu>
2023-10-25 10:43:01 -0400

    added changes

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -5,84 +5,86 @@ import { clearSessionErrors, login } from "../../store/session";
 import "./LoginForm.css";
 
 const LoginForm = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const errors = useSelector((state) => state.errors.session);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const currentUser = useSelector((state) => state.session.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const errors = useSelector((state) => state.errors.session);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const currentUser = useSelector((state) => state.session.user);
 
-    useEffect(() => {
-        return () => {
-            dispatch(clearSessionErrors());
-        };
-    }, [dispatch]);
-
-    useEffect(() => {
-        if (currentUser) {
-            history.push("/dashpage");
-        }
-    }, [currentUser, history]);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(login({ email, password }));
+  useEffect(() => {
+    return () => {
+      dispatch(clearSessionErrors());
     };
+  }, [dispatch]);
 
-    const loginDemo = (e) => {
-        e.preventDefault();
-        dispatch(login({ email: "demoyy@email.com", password: "password" }));
-        history.push("/dashpage");
-    };
+  useEffect(() => {
+    if (currentUser) {
+      history.push("/dashpage");
+    }
+  }, [currentUser, history]);
 
-    return (
-        <div className="center-container">
-            <div className="form-container">
-                <form className="session-form" onSubmit={handleSubmit}>
-                    <h2>Welcome Back</h2>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ email, password }));
+  };
 
-                    <div className="errors">{errors?.email}</div>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        style={{ marginBottom: "-5px" }}
-                        placeholder="Email address"
-                    />
-
-                    <div className="errors">{errors?.password}</div>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                    />
-
-                    <input
-                        type="submit"
-                        className="login-button"
-                        value="Log In"
-                        disabled={!email || !password}
-                    />
-
-                    <input
-                        type="submit"
-                        className="login-button"
-                        value="Demo Log In"
-                        onClick={loginDemo}
-                    />
-                </form>
-                <div>
-                    <span style={{ marginLeft: "75px" }}>
-                        Don't have an account?{" "}
-                        <Link to={`/signup`} className="signup-link">
-                            Sign up
-                        </Link>
-                    </span>
-                </div>
-            </div>
-        </div>
+  const loginDemo = (e) => {
+    e.preventDefault();
+    dispatch(
+      login({ email: "ambreenqadri068@gmail.com", password: "password" })
     );
+    history.push("/dashpage");
+  };
+
+  return (
+    <div className="center-container">
+      <div className="form-container">
+        <form className="session-form" onSubmit={handleSubmit}>
+          <h2>Welcome Back</h2>
+
+          <div className="errors">{errors?.email}</div>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ marginBottom: "-5px" }}
+            placeholder="Email address"
+          />
+
+          <div className="errors">{errors?.password}</div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+
+          <input
+            type="submit"
+            className="login-button"
+            value="Log In"
+            disabled={!email || !password}
+          />
+
+          <input
+            type="submit"
+            className="login-button"
+            value="Demo Log In"
+            onClick={loginDemo}
+          />
+        </form>
+        <div>
+          <span style={{ marginLeft: "75px" }}>
+            Don't have an account?{" "}
+            <Link to={`/signup`} className="signup-link">
+              Sign up
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
