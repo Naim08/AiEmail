c56ce2ade17d7ea19db7b15393940ee2385de280
Naim Miah <mmiah@fordham.edu>
2023-10-31 12:53:45 -0400

    more changes to heroku

--- frontend/src/components/Email/EmailDetails/index.jsx
+++ frontend/src/components/Email/EmailDetails/index.jsx
@@ -11,247 +11,256 @@ import { FormModal } from "../../../context/modal";
 
 import { sendMessage, getMessage } from "../../../store/chatgpt";
 import { sendGmail } from "../../../store/email";
+import TutorialModal from "../../TutorialModal/TutorialModal";
 //EXISTING EMAIL PAGE
 
 const EmailDetails = () => {
-    const { emailId } = useParams();
-    const isLoading = useSelector((state) => state.chatgpt.loading);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const email = useSelector(getEmail(emailId));
-    const emailResponse = useSelector(getMessage);
-    const emailPrompt = useSelector(getEmail(emailId));
-    const [localEmail, setLocalEmail] = useState(email);
-    const [showModal, setShowModal] = useState(false);
-    const [showChatMessage, setShowChatMessage] = useState(false);
+  const { emailId } = useParams();
+  const isLoading = useSelector((state) => state.chatgpt.loading);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const email = useSelector(getEmail(emailId));
+  const emailResponse = useSelector(getMessage);
+  const emailPrompt = useSelector(getEmail(emailId));
+  const [localEmail, setLocalEmail] = useState(email);
+  const [showModal, setShowModal] = useState(false);
+  const [showChatMessage, setShowChatMessage] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const [copyCompleted, setCopyCompleted] = useState(false);
-    const [updateCompleted, setUpdateCompleted] = useState(false);
-    const [sendCompleted, setSendCompleted] = useState(false);
+  const handleModal = () => setShow(!show);
+  const closerModal = (e) => {
+    e.preventDefault();
+    setShow(false);
+  };
 
-    // Add states for icon reset timers
-    const [copyResetTimer, setCopyResetTimer] = useState(null);
-    const [updateResetTimer, setUpdateResetTimer] = useState(null);
-    const [sendResetTimer, setSendResetTimer] = useState(null);
-    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [copyCompleted, setCopyCompleted] = useState(false);
+  const [updateCompleted, setUpdateCompleted] = useState(false);
+  const [sendCompleted, setSendCompleted] = useState(false);
 
+  // Add states for icon reset timers
+  const [copyResetTimer, setCopyResetTimer] = useState(null);
+  const [updateResetTimer, setUpdateResetTimer] = useState(null);
+  const [sendResetTimer, setSendResetTimer] = useState(null);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
-    const error = useSelector((state) => state.emailsReducer.error);
-    const userPreferences = useSelector((state) => state.userPreferenceReducer);
+  const error = useSelector((state) => state.emailsReducer.error);
+  const userPreferences = useSelector((state) => state.userPreferenceReducer);
 
-    useEffect(() => {
-        dispatch(fetchSingleEmail(emailId));
-    }, []);
+  useEffect(() => {
+    dispatch(fetchSingleEmail(emailId));
+  }, []);
 
-    useEffect(() => {
-        if (isFormSubmitted) {
-            const prompt = {
-                subject: localEmail.subject,
-                message: localEmail.message,
-                emailId: emailId,
-            };
+  useEffect(() => {
+    if (isFormSubmitted) {
+      const prompt = {
+        subject: localEmail.subject,
+        message: localEmail.message,
+        emailId: emailId,
+      };
 
-            const options = { ...userPreferences };
-            dispatch(sendMessage({ prompt, options }));
-            
-            // setLocalEmail(email);
-            setIsFormSubmitted(false);
-        }
-    }, [isFormSubmitted]);
+      const options = { ...userPreferences };
+      dispatch(sendMessage({ prompt, options }));
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setLocalEmail({ ...localEmail, [name]: value });
-    };
+      // setLocalEmail(email);
+      setIsFormSubmitted(false);
+    }
+  }, [isFormSubmitted]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(updateEmail({ ...localEmail, id: emailId }));
-        setUpdateCompleted(true); // Set updateCompleted to true
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLocalEmail({ ...localEmail, [name]: value });
+  };
 
-        // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
-        const timer = setTimeout(() => {
-            setUpdateCompleted(false); // Reset updateCompleted to false
-        }, 2000);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(updateEmail({ ...localEmail, id: emailId }));
+    setUpdateCompleted(true); // Set updateCompleted to true
 
-        // Store the timer reference
-        setUpdateResetTimer(timer);
-        setIsFormSubmitted(true);
-    };
+    // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
+    const timer = setTimeout(() => {
+      setUpdateCompleted(false); // Reset updateCompleted to false
+    }, 2000);
 
-    const handleExit = (e) => {
-        e.preventDefault();
-        history.push(`/dashpage`);
-    };
+    // Store the timer reference
+    setUpdateResetTimer(timer);
+    setIsFormSubmitted(true);
+  };
 
-    const handleUserModalShow = (e) => {
-        e.preventDefault();
-        setShowModal(true);
-    };
-    const closeUserPreferModal = () => {
-        setShowModal(false);
-        dispatch(setformSlide("expand"));
-    };
+  const handleExit = (e) => {
+    e.preventDefault();
+    history.push(`/dashpage`);
+  };
 
-    const copyToClipboard = async () => {
-        const emailResponseId = Object.keys(emailResponse)[0];
-        const textToCopy = emailResponse[emailResponseId].response;
-       
-        
-        try {
-            await navigator.clipboard.writeText(textToCopy);
+  const handleUserModalShow = (e) => {
+    e.preventDefault();
+    setShowModal(true);
+  };
+  const closeUserPreferModal = () => {
+    setShowModal(false);
+    dispatch(setformSlide("expand"));
+  };
 
-            setLocalEmail(prevEmail => ({
-                ...prevEmail,
-                message: textToCopy
-            }));
+  const copyToClipboard = async () => {
+    const emailResponseId = Object.keys(emailResponse)[0];
+    const textToCopy = emailResponse[emailResponseId].response;
 
-            setCopyCompleted(true); // Set copyCompleted to true
+    try {
+      await navigator.clipboard.writeText(textToCopy);
 
-            // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
-            const timer = setTimeout(() => {
-                setCopyCompleted(false); // Reset copyCompleted to false
-            }, 2000);
+      setLocalEmail((prevEmail) => ({
+        ...prevEmail,
+        message: textToCopy,
+      }));
 
-            // Store the timer reference
-            setCopyResetTimer(timer);
-           
-        } catch (err) {}
-    };
+      setCopyCompleted(true); // Set copyCompleted to true
 
-    useEffect(() => {
-        // This will log the value of showChatMessage whenever it changes
-    }, [showChatMessage]);
+      // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
+      const timer = setTimeout(() => {
+        setCopyCompleted(false); // Reset copyCompleted to false
+      }, 2000);
 
-    const sendEmailHandler = async (e) => {
-        e.preventDefault();
-        dispatch(sendGmail({ ...localEmail, id: emailId }));
-        setSendCompleted(true); // Set sendCompleted to true
+      // Store the timer reference
+      setCopyResetTimer(timer);
+    } catch (err) {}
+  };
 
-        // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
-        const timer = setTimeout(() => {
-            setSendCompleted(false); // Reset sendCompleted to false
-        }, 2000);
+  useEffect(() => {
+    // This will log the value of showChatMessage whenever it changes
+  }, [showChatMessage]);
 
-        // Store the timer reference
-        setSendResetTimer(timer);
-    };
+  const sendEmailHandler = async (e) => {
+    e.preventDefault();
+    dispatch(sendGmail({ ...localEmail, id: emailId }));
+    setSendCompleted(true); // Set sendCompleted to true
 
-    return (
-        <>
-            <div className="exit-page-btn-div">
-                <div className="exit-wrapper" onClick={handleExit}>
-                    <i className="fa-regular fa-arrow-left exit-icon"></i>
-                    <p className="exit-text">Return to Dashboard</p>
-                </div>
+    // Start a timer to reset the icon state after 2000 milliseconds (2 seconds)
+    const timer = setTimeout(() => {
+      setSendCompleted(false); // Reset sendCompleted to false
+    }, 2000);
+
+    // Store the timer reference
+    setSendResetTimer(timer);
+  };
+
+  return (
+    <>
+      <div className="exit-page-btn-div">
+        <div className="exit-wrapper" onClick={handleExit}>
+          <i className="fa-regular fa-arrow-left exit-icon"></i>
+          <p className="exit-text">Return to Dashboard</p>
+        </div>
+      </div>
+
+      {showModal && (
+        <div className="form-modal-outer">
+          <FormModal onClose={closeUserPreferModal}>
+            <button className="close-modal-btn" onClick={closeUserPreferModal}>
+              X
+            </button>
+            <UserPreferModal setShowModal={setShowModal} />
+          </FormModal>
+        </div>
+      )}
+      <div className="main-container">
+        <div className="new-email-form-container">
+          <div style={{ color: "white" }}>{error ? error : ""}</div>
+          <form onSubmit={handleSubmit} className="new-email-form">
+            <div className="new-email-to">
+              <input
+                type="text"
+                className="form-input"
+                name="to"
+                placeholder="To"
+                value={localEmail ? localEmail.to : ""}
+                onChange={handleChange}
+              />
             </div>
+            <div className="dotted-line"></div>
+            <div className="new-email-subject">
+              <input
+                type="text"
+                className="form-input"
+                name="subject"
+                value={localEmail ? localEmail.subject : ""}
+                onChange={handleChange}
+                placeholder="Subject"
+              />
+            </div>
+            <div className="dotted-line"></div>
+            <div>
+              <button className="user-prefer-btn" onClick={handleUserModalShow}>
+                <i class="fa-sharp fa-light fa-sliders"></i>
+              </button>
+            </div>
+            <div className="new-email-body" style={{ flexGrow: 1 }}>
+              <textarea
+                name="message"
+                value={localEmail ? localEmail.message : ""}
+                onChange={handleChange}
+                placeholder="Message"
+                style={{ height: "100%" }}
+              />
+            </div>
+            <div className="new-email-form-btn">
+              <button type="submit" onClick={() => setShowChatMessage(true)}>
+                {updateCompleted ? (
+                  <i className="fa-solid fa-check"></i> // Checkmark icon
+                ) : (
+                  "update"
+                )}
+              </button>
+              <button type="submit" onClick={sendEmailHandler}>
+                {sendCompleted ? (
+                  <i className="fa-solid fa-check"></i> // Checkmark icon
+                ) : (
+                  "send"
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+        <button onClick={handleModal} className="neumorphic">
+          Tutorial
+        </button>
+        {show && (
+          // <Modal centered show={show} onHide={handleModal} style={{height: "650px !important", width: "1000px !important"}} dialogClassName="custom-modal-content">
+          <div className="tutorialModal">
+            <TutorialModal />
+            <div className="close-modal-btn center-btn">
+              <button className="neumorphic" onClick={closerModal}>
+                Close
+              </button>
+            </div>
+          </div>
 
-            {showModal && (
-                <div className="form-modal-outer">
-                    <FormModal onClose={closeUserPreferModal}>
-                        <button
-                            className="close-modal-btn"
-                            onClick={closeUserPreferModal}
-                        >
-                            X
-                        </button>
-                        <UserPreferModal setShowModal={setShowModal} />
-                    </FormModal>
-                </div>
+          //    </Modal>
+        )}
+        <div className="chat-message">
+          {isLoading ? (
+            <div className="loading-dots">
+              <span>Loading</span>
+              <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </div>
+          ) : emailResponse ? ( // Check if emailResponse is truthy
+            Object.values(emailResponse).map((message, idx) => (
+              <MessageComponent key={idx} message={message} />
+            ))
+          ) : (
+            <div></div> // Default message or component when emailResponse is falsy
+          )}
+          <button className="copy-button" onClick={copyToClipboard}>
+            {copyCompleted ? (
+              <i className="fa-solid fa-check"></i> // Checkmark icon
+            ) : (
+              <i className="fa-solid fa-copy"></i> // Original copy icon
             )}
-            <div className="main-container">
-                <div className="new-email-form-container">
-                    <div style={{ color: "white" }}>{error ? error : ""}</div>
-                    <form onSubmit={handleSubmit} className="new-email-form">
-                        <div className="new-email-to">
-                            <input
-                                type="text"
-                                className="form-input"
-                                name="to"
-                                placeholder="To"
-                                value={localEmail ? localEmail.to : ""}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="dotted-line"></div>
-                        <div className="new-email-subject">
-                            <input
-                                type="text"
-                                className="form-input"
-                                name="subject"
-                                value={localEmail ? localEmail.subject : ""}
-                                onChange={handleChange}
-                                placeholder="Subject"
-                            />
-                        </div>
-                        <div className="dotted-line"></div>
-                        <div>
-                            <button
-                                className="user-prefer-btn"
-                                onClick={handleUserModalShow}
-                            >
-                                <i class="fa-sharp fa-light fa-sliders"></i>
-                            </button>
-                        </div>
-                        <div className="new-email-body" style={{ flexGrow: 1 }}>
-                            <textarea
-                                name="message"
-                                value={localEmail ? localEmail.message : ""}
-                                onChange={handleChange}
-                                placeholder="Message"
-                                style={{ height: "100%" }}
-                            />
-                        </div>
-                        <div className="new-email-form-btn">
-                            <button
-                                type="submit"
-                                onClick={() => setShowChatMessage(true)}
-                            >
-                                {updateCompleted ? (
-                                    <i className="fa-solid fa-check"></i> // Checkmark icon
-                                ) : (
-                                    "update"
-                                )}
-                            </button>
-                            <button type="submit" onClick={sendEmailHandler}>
-                                {sendCompleted ? (
-                                    <i className="fa-solid fa-check"></i> // Checkmark icon
-                                ) : (
-                                    "send"
-                                )}
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div className="chat-message">
-
-                    {isLoading ? (
-                        <div className="loading-dots">
-                            <span>Loading</span>
-                            <span>.</span>
-                            <span>.</span>
-                            <span>.</span>
-                        </div>
-                    ) : (
-                        emailResponse ? ( // Check if emailResponse is truthy
-                            Object.values(emailResponse).map((message, idx) => (
-                                <MessageComponent key={idx} message={message} />
-                            ))
-                        ) : (
-                            <div></div> // Default message or component when emailResponse is falsy
-                        )
-                    )}
-                    <button className="copy-button" onClick={copyToClipboard}>
-                        {copyCompleted ? (
-                            <i className="fa-solid fa-check"></i> // Checkmark icon
-                        ) : (
-                            <i className="fa-solid fa-copy"></i> // Original copy icon
-                        )}
-                    </button>
-                </div>
-            </div>
-        </>
-    );
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default EmailDetails;
